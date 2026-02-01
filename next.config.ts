f845fe947718632dc226bf260c5438bb5e83c8e6
swayam0815 <swayam0815@gmail.com>
2026-02-01 12:41:09 -0500

    update config file to resolve errors

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
